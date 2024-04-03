a4760a30782710f4efd346f6fe63a459d93070f7
baekwinter <kksbiy0012@gmail.com>
2024-04-03 21:50:03 +0900

    [AddFix]_02_몬스터생성
    오브젝트 풀링을 이용한 몬스터 생성

---
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    //..프리팹 저장할 변수(배열)
+    public GameObject[] prefabs;
+
+    // 풀 담당할 리스트 필요!!
+    List<GameObject>[] pools;
+
+    private void Awake()
+    {
+        pools = new List<GameObject>[prefabs.Length];
+
+        for(int index = 0; index < pools.Length; index++) 
+        {
+            pools[index] = new List<GameObject>();
+        }
+
+    }
+
+    public GameObject Get(int index)
+    {
+        GameObject select = null;
+        // 1. 다른 스크립트에서 Get을 쓸 경우, 선택한 풀의 놀고 있는(비활성화된) 게임오브젝트 접근 
+
+        foreach (GameObject item  in pools[index])
+        {
+            if (!item.activeSelf)
+            {
+                // 2. 만약에 해당 부분을 발견했을 경우, 발견하면 select 변수에 할당
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        } 
+        // 3. 못찾았다면?
+        if (!select) 
+        {
+            //-> 새롭게 생성하여 select 변수에 할당
+
+            // poolManager 부모로 설정
+            select = Instantiate(prefabs[index],transform);
+            pools[index].Add(select); //풀에 등록하기
+
+        }
+
+
+        return select;
+    }
+}
